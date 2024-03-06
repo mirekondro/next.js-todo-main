5ad79da0c16a63f9082c930847f60cbbe477d81d
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-03-06 18:51:38 +0100

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
   return (
     <>
       <header className="flex justify-between items-center pb-5">
-        <h1 className="text-2xl underline underline-offset-4 ">Todos</h1>
+        <h1 className="text-2xl underline underline-offset-4 ">Todo's</h1>
         <Link
           href="/new"
           className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 outline-none"
