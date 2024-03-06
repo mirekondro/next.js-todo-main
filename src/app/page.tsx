ab093109522bcde262d50133ffb4be61546b0fd5
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-03-06 19:05:29 +0100

    ui edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
           href="/new"
           className="border border-slate-300 text-slate-300 rounded px-2 py-1 hover:bg-zinc-700 focus-within:bg-zinc-700 outline-none"
         >
-          New
+          ADD NEW
         </Link>
       </header>
       <ul className="pl-4">
